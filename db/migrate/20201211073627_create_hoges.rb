d13f0c50dbb840c1dcc6ea07f706996df806af86
somemorishiro <73918246+somemorishiro@users.noreply.github.com>
2020-12-11 16:38:28 +0900

    Revert "モデル作成したよ"
    This reverts commit 7c8d9c03e5df9452463b1564debf89ae23f36f23.

--- db/migrate/20201211073627_create_hoges.rb
+++
@@ -1,8 +0,0 @@
-class CreateHoges < ActiveRecord::Migration[6.0]
-  def change
-    create_table :hoges do |t|
-
-      t.timestamps
-    end
-  end
-end
--- spec/factories/hoges.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :hoge do
-    
-  end
-end
